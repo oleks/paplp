766acff1b7d919ead2d442b4f3cb83c4a723d37a
Oleksandr Shturmov <oleks@diku.dk>
2012-02-17 17:16:12 +0100

    scan, now with offset, done very soon..

--- scan/scan.cu
+++ scan/scan.cu
@@ -28,30 +28,30 @@ void printArray(int count, T const& array)
   print1D("[", count, array, ", ", "]");
 }
 
-#define BLOCK_SIZE 4
-#define LOG(value) ((int)log2((double)(value)))
-#define ODD(value) ((value)&0x01)
+#define BLOCK_SIZE 8
 #define DOUBLE(value) ((value) << 1)
 #define HALF(value) ((value) >> 1)
+#define GLOBAL_ID (blockIdx.x * blockDim.x + threadIdx.x)
 
 template<typename T>
-__global__ void prefixScan(int count, T *values)
+__global__ void scan(int count, int offset, T *values)
 {
-	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	int id = GLOBAL_ID;
+  int offset_id = id * offset + (offset == 1 ? 0 : offset - 1);
 
   __shared__ T block_values[BLOCK_SIZE];
-  block_values[threadIdx.x] = values[id];
+  block_values[threadIdx.x] = values[offset_id];
   __syncthreads();
 
   int i, id1 = id + 1;
-  for ( i = 2; i < count; i = DOUBLE(i) )
+  for ( i = 2; i <= count; i = DOUBLE(i) )
   {
     if (id1 % i == 0)
     {
       block_values[threadIdx.x] += block_values[threadIdx.x - HALF(i)];
     }
+    __syncthreads();
   }
-  __syncthreads();
 
   for ( i = HALF(BLOCK_SIZE); i >= 2; i = HALF(i) )
   {
@@ -59,21 +59,18 @@ __global__ void prefixScan(int count, T *values)
     {
       block_values[threadIdx.x + HALF(i)] += block_values[threadIdx.x]; 
     }
+    __syncthreads();
   }
-  __syncthreads();
 
-  values[id] = block_values[threadIdx.x];
+  values[offset_id] = block_values[threadIdx.x];
 }
 
 int main()
 {
   int hostValues [] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
   const int COUNT = 16;
-  const int height = LOG(COUNT/BLOCK_SIZE);
 
   cudaDeviceReset();
-
-  printf("Height: %d\n", height);
 
   const int size = sizeof(int) * COUNT;
 
@@ -82,7 +79,9 @@ int main()
 
   cudaMemcpy(deviceValues, hostValues, size, cudaMemcpyHostToDevice);
 
-  prefixScan<<<4,4>>>(COUNT, deviceValues);
+  scan<<<2,BLOCK_SIZE>>>(COUNT, 1, deviceValues);
+
+  scan<<<1,2>>>(COUNT/BLOCK_SIZE, BLOCK_SIZE, deviceValues);
 
   cudaMemcpy(hostValues, deviceValues, size, cudaMemcpyDeviceToHost);
 
